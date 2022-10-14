fd7be1937c48cc488e1a10abc765de45eac8d295
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-14 18:59:11 -0300

    Soma de Pares Consecutivos
    O programa deve ler um valor inteiro X indefinidas vezes. (O programa irá parar quando o valor de X for igual a 0). Para cada X lido, imprima a soma dos 5 pares consecutivos a partir de X, inclusive o X , se for par. Se o valor de entrada for 4, por exemplo, a saída deve ser 40, que é o resultado da operação: 4+6+8+10+12, enquanto que se o valor de entrada for 11, por exempo, a saída deve ser 80, que é a soma de 12+14+16+18+20.
    
    Entrada
    O arquivo de entrada contém muitos valores inteiros. O último valor do arquivo é zero.
    
    Saída
    Imprima a saida conforme a explicação acima e o exemplo abaixo.

---
+++ Program1159/Program1159/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            // se for impar, some mais 1
+            while (x != 0)
+            {
+                if (x % 2 != 0)
+                {
+                    x = x + 1;                    
+                }
+
+                int soma = x + x + 2 + x + 4 + x + 6 + x + 8;
+                Console.WriteLine(soma);
+
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
