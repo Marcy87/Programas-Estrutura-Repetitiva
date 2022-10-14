b0ae5dbedfbb10a3b347fb84883337613ad4fb76
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-14 17:31:53 -0300

    Tipo de Combustível
    Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até que seja válido). O programa será encerrado quando o código informado for o número 4.
    
    Entrada
    A entrada contém apenas valores inteiros e positivos.
    
    Saída
    Deve ser escrito a mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme exemplo.

---
+++ Program1134/Program1134/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Program1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contAlcool = 0;
+            int contGasolina = 0;
+            int contDiesel = 0;
+
+            int tipoComb = 0;
+
+            while (tipoComb != 4)
+            {
+                if (tipoComb == 1)
+                {
+                    contAlcool++;
+                }
+                else if (tipoComb == 2)
+                {
+                    contGasolina++;
+                }
+                else if (tipoComb == 3)
+                {
+                    contDiesel++;
+                }
+                tipoComb = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + contAlcool);
+            Console.WriteLine("Gasolina: " + contGasolina);
+            Console.WriteLine("Diesel: " + contDiesel);
+        }
+    }
+}
