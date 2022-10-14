296fe062cc3f68a9fe02207543908fbd8f218b15
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-14 16:12:33 -0300

    Grenais
    A Federação Gaúcha de Futebol contratou você para escrever um programa para fazer uma estatística do resultado de vários GRENAIS. Escreva um programa para ler o número de gols marcados pelo Inter e pelo Grêmio em um GRENAL. Logo após escrever a mensagem "Novo grenal (1-sim 2-nao)" e solicitar uma resposta. Se a resposta for 1, o algoritmo deve ser executado novamente solicitando o número de gols marcados pelos times em uma nova partida, caso contrário deve ser encerrado imprimindo:
    
    - Quantos GRENAIS fizeram parte da estatística.
    - O número de vitórias do Inter.
    - O número de vitórias do Grêmio.
    - O número de Empates.
    - Uma mensagem indicando qual o time que venceu o maior número de GRENAIS (ou "Nao houve vencedor", caso termine empatado).
    
    Entrada
    O arquivo de entrada contém 2 valores inteiros, correspondentes aos gols marcados pelo Inter e pelo Grêmio respectivamente. Em seguida háverá um inteiro (1 ou 2), correspondente à repetição do programa.
    
    Saída
    Após cada leitura dos gols, deve ser impressa a mensagem "Novo grenal (1-sim 2-nao)". Quando o algoritmo for encerrado devem ser mostradas as estatísticas conforme a descrição apresentada acima. Obs: a palavra "Gremio" deve ser impressa sem acento, conforme o exemplo abaixo.

---
+++ Program1131/Program1131/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Program1131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod = 1;
+            int contInter = 0;
+            int contGremio = 0;
+            int grenais = 0;
+            int empates = 0;
+
+            while (cod == 1)
+            {
+                string[] gols = Console.ReadLine().Split(' ');
+                                
+                int golsInter = int.Parse(gols[0]);
+                int golsGremio = int.Parse(gols[1]);
+                
+                if (golsInter > golsGremio)
+                {
+                    contInter++;
+                }
+                else if (golsInter < golsGremio)
+                {
+                    contGremio++;
+                }
+                else
+                {
+                    empates++;
+                }
+
+                grenais++;
+
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                cod = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(grenais + " grenais");
+            Console.WriteLine("Inter:" + contInter);
+            Console.WriteLine("Gremio:" + contGremio);
+            Console.WriteLine("Empates:" + empates);
+
+            if (contInter > contGremio)
+            {
+                Console.WriteLine("Inter venceu mais");
+            }
+            else if (contInter < contGremio)
+            {
+                Console.WriteLine("Gremio venceu mais");
+            }
+            else
+            {
+                Console.WriteLine("Nao houve vencedor");
+            }
+        }
+    }
+}
