846266d3614a2512a4f4bc67de37e7cd70a1fe86
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-14 17:44:07 -0300

    Idades
    Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém o valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos.
    
    Entrada
    A entrada contém um número indeterminado de inteiros. A entrada será encerrada quando um valor negativo for lido.
    
    Saída
    A saída contém um valor correspondente à média de idade dos indivíduos.
    
    A média deve ser impressa com dois dígitos após o ponto decimal.

---
+++ Program1154/Program1154/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            int soma = 0;
+            int cont = 0;
+            double media;
+
+            while (idade > 0)
+            {
+                soma += idade;
+                cont++;
+                idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            media = (double) soma / cont;
+
+            Console.WriteLine(media.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
