140bd402fb68e98a997943f7eea4c86a7cbf985f
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-17 17:51:14 -0300

    Médias Ponderadas
    Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5.
    
    Entrada
    O arquivo de entrada contém um valor inteiro N na primeira linha. Cada N linha a seguir contém um caso de teste com três valores com uma casa decimal cada valor.
    
    Saída
    Para cada caso de teste, imprima a média ponderada dos 3 valores, conforme exemplo abaixo.

---
+++ Program1079/Program1079/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1079
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                double x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+                double y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                double z = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+                double media = ((x * 2.0) + (y * 3.0) + (z * 5.0)) / 10.0;
+                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
