f9ba8d49e5f290ae9ab0eaba90b94cdd4ed35212
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-17 19:26:20 -0300

    Experiências
    Maria acabou de iniciar seu curso de graduação na faculdade de medicina e precisa de sua ajuda para organizar os experimentos de um laboratório o qual ela é responsável. Ela quer saber no final do ano, quantas cobaias foram utilizadas no laboratório e o percentual de cada tipo de cobaia utilizada.
    
    Este laboratório em especial utiliza três tipos de cobaias: sapos, ratos e coelhos. Para obter estas informações, ela sabe exatamente o número de experimentos que foram realizados, o tipo de cobaia utilizada e a quantidade de cobaias utilizadas em cada experimento.
    
    Entrada
    A primeira linha de entrada contém um valor inteiro N que indica os vários casos de teste que vem a seguir. Cada caso de teste contém um inteiro Quantia (1 ≤ Quantia ≤ 15) que representa a quantidade de cobaias utilizadas e um caractere Tipo ('C', 'R' ou 'S'), indicando o tipo de cobaia (R:Rato S:Sapo C:Coelho).
    
    Saída
    Apresente o total de cobaias utilizadas, o total de cada tipo de cobaia utilizada e o percentual de cada uma em relação ao total de cobaias utilizadas, sendo que o percentual deve ser apresentado com dois dígitos após o ponto.

---
+++ Program1094/Program1094/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1094
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int ratos = 0;
+            int sapos = 0;
+            int coelhos = 0;
+            int cobaias = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+
+                int quantia = int.Parse(vet[0]);
+                char tipo = char.Parse(vet[1]);
+
+                if (tipo == 'C')
+                {
+                    coelhos += quantia;
+                } else if (tipo == 'R')
+                {
+                    ratos += quantia;
+                } else
+                {
+                    sapos += quantia;
+                }
+
+                cobaias += quantia;
+            }
+
+            double porcentagemCoelhos = (double) coelhos / cobaias * 100.0;
+            double porcentagemRatos = (double) ratos / cobaias * 100.0;
+            double porcentagemSapos = (double) sapos / cobaias * 100.0;
+
+            Console.WriteLine("Total: " + cobaias + " cobaias");
+            Console.WriteLine("Total de coelhos: " + coelhos);
+            Console.WriteLine("Total de ratos: " + ratos);
+            Console.WriteLine("Total de sapos: " + sapos);
+            Console.WriteLine("Percentual de coelhos: " + porcentagemCoelhos.ToString("F2", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Percentual de ratos: " + porcentagemRatos.ToString("F2", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Percentual de sapos: " + porcentagemSapos.ToString("F2", CultureInfo.InvariantCulture) + " %");
+        }
+    }
+}
