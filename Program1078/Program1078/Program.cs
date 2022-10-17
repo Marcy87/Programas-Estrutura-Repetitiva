847ce83ffa4d004b79308f377bb9ccb14f70892e
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-17 17:32:51 -0300

    Tabuada
    Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
    1 x N = N      2 x N = 2N        ...       10 x N = 10N
    
    Entrada
    A entrada contém um valor inteiro N (2 < N < 1000).
    
    Saída
    Imprima a tabuada de N, conforme o exemplo fornecido.

---
+++ Program1078/Program1078/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int produto;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                produto = i * n;
+                Console.WriteLine(i + " x " + n + " = " + produto);
+            }
+        }
+    }
+}
