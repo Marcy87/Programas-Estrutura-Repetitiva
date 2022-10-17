a65f0e1223e4c17efdeecbf776fb538f51a12694
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-17 13:54:05 -0300

    Intervalo 2
    Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
    Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando essas informações.
    
    Entrada
    A primeira linha da entrada contém um valor inteiro N (N < 10000), que indica o número de casos de teste.
    Cada caso de teste a seguir é um valor inteiro X (-107 < X <107).
     
    
    Saída
    Para cada caso, imprima quantos números estão dentro (in) e quantos valores estão fora (out) do intervalo.

---
+++ Program1072/Program1072/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program1072
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, contIn = 0, contOut = 0;
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) 
+            { 
+                x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20)
+                {
+                    contIn++;
+                }
+                else
+                {
+                    contOut++;
+                }
+            }
+
+            Console.WriteLine(contIn + " in");
+            Console.WriteLine(contOut + " out");
+        }
+    }
+}
