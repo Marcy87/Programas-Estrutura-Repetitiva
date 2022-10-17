f6490f56a5a1325697c1aa4656906618b5828a09
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-17 10:47:32 -0300

    Números Ímpares
    Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
    
    Entrada
    O arquivo de entrada contém 1 valor inteiro qualquer.
    
    Saída
    Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.

---
+++ Program1067/Program1067/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            
+            if (x % 2 != 0)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
