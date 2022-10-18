5c24ae0b478fed7780cbe6c53b71a8f415c615b0
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-18 15:30:18 -0300

    Fatorial Simples
    Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
    
    Entrada
    A entrada contém um valor inteiro N (0 < N < 13).
    
    Saída
    A saída contém um valor inteiro, correspondente ao fatorial de N.

---
+++ Program1153/Program1153/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Program1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int prod = 1;
+
+            for (int i = 1; i < N; i++)
+            {
+                prod *= (N - i);
+            }
+
+            int total = N * prod;
+
+            Console.WriteLine(total);
+        }
+    }
+}
