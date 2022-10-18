b4a643317c7ef59a7027180d561c95ae58c20315
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-18 11:41:40 -0300

    PUM
    Escreva um programa que leia um valor inteiro N. Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
    
    Entrada
    O arquivo de entrada contém um número inteiro positivo N.
    
    Saída
    Imprima a saída conforme o exemplo fornecido.
    
    Exemplo de Entrada	         Exemplo de Saída
    7                                                1 2 3 PUM                                                 
                                                      5 6 7 PUM
                                                      9 10 11 PUM
                                                    13 14 15 PUM
                                                    17 18 19 PUM
                                                    21 22 23 PUM
                                                    25 26 27 PUM

---
+++ Program1142/Program1142/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Program1142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int cont = 0;
+
+            for (int i = 0; i < N; i++)
+            { 
+                for (int j = 0; j < 3; j++)
+                {
+                    cont += 1;
+                    Console.Write(cont + " ");
+                }
+                Console.WriteLine("PUM");
+                cont += 1;
+            }
+
+            // ou
+
+            /* int n = int.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= n; i++) {
+
+                int segundo = inicio + 1;
+                int terceiro = inicio + 2;
+                Console.WriteLine(inicio + " " + segundo + " " + terceiro + " PUM");
+
+                inicio = inicio + 4;
+            }*/
+        }
+    }
+}
