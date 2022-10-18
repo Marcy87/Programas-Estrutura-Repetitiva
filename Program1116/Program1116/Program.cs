a2b2b95e7e01d7f6910cdbfe1835bcc1854338ad
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-18 10:58:56 -0300

    Dividindo X por Y
    Escreva um algoritmo que leia 2 números e imprima o resultado da divisão do primeiro pelo segundo. Caso não for possível mostre a mensagem “divisao impossivel” para os valores em questão.
    
    Entrada
    A entrada contém um número inteiro N. Este N será a quantidade de pares de valores inteiros (X e Y) que serão lidos em seguida.
    
    Saída
    Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja possível efetuar o cálculo.
    
    Obs.: Cuide que a divisão entre dois inteiros em algumas linguagens como o C e C++ gera outro inteiro. Utilize cast :)

---
+++ Program1116/Program1116/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+
+                int x = int.Parse(vet[0], CultureInfo.InvariantCulture);
+                int y = int.Parse(vet[1], CultureInfo.InvariantCulture);
+              
+                if (y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                } else
+                {
+                    double divisao;
+
+                    divisao = (double) x / y;
+
+                    Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
