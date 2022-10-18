eb0ced13f4635a647ef7844b2b62156c295bf3fb
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-18 14:03:06 -0300

    Quadrado e ao Cubo
    Escreva um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
    
    Entrada
    O arquivo de entrada contém um número inteiro positivo N.
    
    Saída
    Imprima a saída conforme o exemplo fornecido.
    
    Exemplo de Entrada	Exemplo de Saída
    5                                         1 1 1
                                               2 4 8
                                               3 9 27
                                               4 16 64
                                               5 25 125

---
+++ Program1143/Program1143/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Program1143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write(i + " ");
+
+                for (int j = 0; j < 1; j++)
+                {
+                    int mul = i * i;
+                    Console.Write(mul + " ");
+
+                    for (int z = 0; z < 1; z++)
+                    {
+                        int prod = i * mul;
+                        Console.Write(prod);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // OU
+            /*int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                int primeiro = i;
+                int segundo = i * i;
+                int terceiro = i * i * i;
+                Console.WriteLine($"{primeiro} {segundo} {terceiro}");
+            }*/
+        }
+    }
+}
